770d73a30fc60362cdb2070a0c34bac865371942
Kosi Akporji <kosiazom@gmail.com>
2020-05-22 16:40:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   
   name_hash.each do |key, value|
+    binding.pry
     if lowest_key == nil ||  value < lowest_value
       
       lowest_value = value
